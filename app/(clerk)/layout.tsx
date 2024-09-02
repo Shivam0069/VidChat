77d9e763241b3d09a63b8f8acba2dd19ed49871d
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-09-03 01:28:03 +0530

    Complete Clerk authentication with navbar

---
+++ app/(clerk)/layout.tsx
@@ -0,0 +1,6 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screen flex justify-center items-center">{children}</div>
+  );
+};
+export default ClerkLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import NavBar from "@/components/layout/NavBar";
+import Container from "@/components/layout/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <main className="flex flex-col bg-secondary min-h-screen">
+            <NavBar />
+            <Container>{children}</Container>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
